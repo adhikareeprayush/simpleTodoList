e711c3c56516a21d4d0bcce3064cc5df4d48d2a4
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2024-01-05 18:23:01 +0545

    Update script.js

--- script.js
+++ script.js
@@ -47,50 +47,19 @@ document.getElementById('add').addEventListener('click', function() {
         if (checkBox.classList.contains('fa-square')) {
             checkBox.classList.remove('fa-square');
             checkBox.classList.add('fa-check-square');
-            item.style.textDecoration = 'line-through';
-            item.style.color='#fff';
+            let span = item.parentElement.querySelector('.text');
+            span.style.textDecoration = 'line-through';
         } else {
             checkBox.classList.remove('fa-check-square');
             checkBox.classList.add('fa-square');
-            item.style.textDecoration = 'none';
-            item.textContent.style.color = '#FAF0E6';
+            let span = item.parentElement.querySelector('.text');
+            span.style.textDecoration = 'none';
         }
     });
 
     // Add event listener to the newly created delete button
     del.addEventListener('click', function() {
         item.remove();
-
-        if (list.children.length === 1) {
-            document.getElementById('iAmEmpty').style.display = 'block';
-        }
-    });
-});
-
-
-var check = document.querySelectorAll('.fa-regular');
-check.forEach(function(item) {
-    item.addEventListener('click', function() {
-        if (item.classList.contains('fa-square')) {
-            item.classList.remove('fa-square');
-            item.classList.add('fa-check-square');
-            item.parentElement.style.textDecoration = 'line-through';
-            item.style.color = '#FAF0E6';
-        } else {
-            item.classList.remove('fa-check-square');
-            item.classList.add('fa-square');
-            item.parentElement.style.textDecoration = 'none';
-            item.style.color='#fff';
-        }
-    });
-});
-
-
-
-var deleteBtn = document.querySelectorAll('.del');
-deleteBtn.forEach(function(item) {
-    item.addEventListener('click', function() {
-        item.parentNode.remove();
 
         if (list.children.length === 1) {
             document.getElementById('iAmEmpty').style.display = 'block';
